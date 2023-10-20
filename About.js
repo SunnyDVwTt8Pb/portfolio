bc5b536f7d207b4dbbbf702c339d7c7875368ce9
SunnyDVwTt8Pb <123383802+SunnyDVwTt8Pb@users.noreply.github.com>
2023-10-20 11:26:58 +0530

    Update About.js

--- About.js
+++ About.js
@@ -5,7 +5,7 @@ const About = () => {
     <section>
       <h2>About Me</h2>
       <p>
-      Hi there??.I am Lakshmi prasanna and I'm currently pursuing a BTECH in Computer Science Engineering with specialization in Artificial Intelligence and Machine Learning from VIT-AP University. I am interested and looking forward to collaborate one the following topics Web Development, Artificial Intelligence, Machine Learning etc.
+      Hi there??.I am Konduru Praveen Kumar and I'm currently pursuing a BTECH in Computer Science Engineering (core) from VIT-AP University. I am interested and looking forward to collaborate one the following topics Web Development, Artificial Intelligence, Machine Learning etc.
 
 Along with my academics, I have actively participated in various events such as coding competitions, hackathons. These experiences have helped me to hone my skills and have provided me with the opportunity to work on innovative projects with diverse teams.I'm eager to learn new skills and technologies. 
 I am seeking to connect with professionals and experts in the field of computer science to learn and explore potential opportunities for my career growth.
